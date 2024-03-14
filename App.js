6d9f684148e4366a2d8b95da6f96206fc9478c0c
Brianna Carlisle <103350106+bcarlisle22@users.noreply.github.com>
2024-03-14 08:22:28 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import './App.css';
-import Search from './components/search';
 
-const handleOnSearchChange = (searchData) =>{
-  console.log(searchData)
-}
+import './App.css';
+import Search from './components/search/search';
+import CurrentWeather from './components/current-weather/current-weather';
+import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
+import { useState } from 'react';
 
 function App() {
+
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [currentForecast, setCurrentForecast] = useState(null);
+
+  const handleOnSearchChange = (searchData) =>{   // was initally outside of the function
+    const[lat, lon] = searchData.value.split(" ");
+   
+    const currentWeatherFetch = fetch (`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY})}`);
+    const currentForecastFetch = fetch (`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY})}`);
+
+    Promise.all([currentWeatherFetch, currentForecastFetch])
+    .then(async(response)=> {
+      const weatherResponse = await response [0].json();
+      const forecastResponse = await response [1].json();
+
+      setCurrentWeather({ city: searchData.label , ...weatherResponse});
+      setCurrentForecast({ city: searchData.label , ...forecastResponse});
+
+    })
+
+    .catch((err) => console.log(err));
+  }
+
+  console.log(currentWeather);
+  console.log(currentForecast);
+
   return (
     <div className="container">
       <Search onSearchChange = {handleOnSearchChange}/>
--- api.js
+++ api.js
@@ -1,10 +1,14 @@
-export const  geoApiOptions = {
+
+
+export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'd153d0a4demshde0002c8102c51ep14a90ejsn926d6a05e24e',
+		'X-RapidAPI-Key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H', //updated api key - application works fine
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions";
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'; //updated url 
 
+export const WEATHER_API_URL = 'https;//api.openweatherwap.org/data/2.5';
+export const WEATHER_API_KEY ='9ad1336a393d8da13d0d523979ee1570';
