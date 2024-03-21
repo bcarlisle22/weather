62ffcf19140163cf7db72cdfedc26c4d28a5595e
Brianna Carlisle <103350106+bcarlisle22@users.noreply.github.com>
2024-03-20 22:27:30 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "./App.css";
+import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import Forecast from "./components/current-weather/forecast/forecast";
+import { useState } from "react";
+
+function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    // was initally outside of the function
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  console.log(currentWeather);
+  console.log(forecast);
+
+  return (
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast &&<Forecast data={forecast}/>} 
+    </div> // current-weather.js content was not displaying, fix: forgot to add <CurrentWeather/> to return
+           // forecast.js error: "Cannot read properties of null (reading.list) fix: Capitalize "F" in forecast on line 41
+  );
+}
+
+export default App;
---
+++ src/api.js
@@ -0,0 +1,14 @@
+
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H', //updated api key - application works fine
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'; //updated url 
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'; //updated url 
+export const WEATHER_API_KEY ="58ebb3ff1cf28bab62fa83a63de5a348"; //updated key 
